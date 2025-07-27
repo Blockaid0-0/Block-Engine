c9c2a89c0dc4febf5bd5d56cf2eddd79412411f9
blockaid <blkthegrt0609@gmail.com>
2025-07-27 15:52:30 -0500

    First Commit

---
+++ main.cpp
@@ -0,0 +1,256 @@
+#include <cmath>
+#include <iostream>
+#include "glad/glad.h"
+#include <GLFW/glfw3.h>
+#include "headers/shaders.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+bool wireFrame = false;
+unsigned int wf = 0x1B02;
+int SWIDTH = 640, SHEIGHT = 480;
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraTarget = glm::vec3(0.0f, 0.0f, 0.0f);
+glm::vec3 cameraDirection = glm::normalize(cameraPos - cameraTarget);
+glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp    = glm::vec3(0.0f, 1.0f,  0.0f);
+glm::vec3 cameraRight = glm::vec3(1.0f, 0.0f, 0.0f);
+
+float deltaTime = 0.0f;
+float lastFrame = 0.0f;
+float s = 1.0f;
+float movSpeed = 2.5f;
+void input(GLFWwindow* window);
+float vertices[] = {
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+     0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+     0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+    -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+    -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+};
+unsigned int indices[] = {
+    0, 1, 3,
+    1, 2, 3,
+};
+
+float borderColors[] = {1.0f, 1.0f, 0.0f, 1.0f};
+void framebuffer(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
+
+bool firstMouse = true;
+float yaw   = -90.0f;
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+
+
+int main() {
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    GLFWwindow* window = glfwCreateWindow(SWIDTH, SHEIGHT, "OpenGL Demo", nullptr, nullptr);
+    if (window == nullptr) {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer);
+    Shader ourShader("C:/Users/blkth/CLionProjects/OPENGL/vertexShaderSource.vert", "C:/Users/blkth/CLionProjects/OPENGL/fragmentShaderSource.frag");
+    unsigned int vao, vbo, ebo;
+    glGenVertexArrays(1, &vao);
+    glGenBuffers(1, &vbo);
+    glGenBuffers(1, &ebo);
+    glBindVertexArray(vao);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)nullptr);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)(3*sizeof(float)));
+    glEnableVertexAttribArray(1);
+    glBindVertexArray(0);
+    int width, height, nrChannels;
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, borderColors);
+        stbi_set_flip_vertically_on_load(true);
+
+    unsigned char *data = stbi_load("C:/Users/blkth/CLionProjects/OPENGL/textures/joe.png", &width, &height, &nrChannels, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+    glEnable(GL_DEPTH_TEST);
+   // glEnable(GL_CULL_FACE);
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330 core");
+    float ang = 0;
+    float x = 0, y = 0.001f, z = 0;
+    float px = 0, py = 0, pz = 0;
+    float currentAngle = 0.0f;
+    float lastFrme = 0.0f;
+    float fov = 60.0f;
+    while (!glfwWindowShouldClose(window)) {
+        input(window);
+        auto currentFrame = (float)glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+        //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        //glfwSetCursorPosCallback(window, GLFWcursorposfun(mouse_callback));
+        //Gui Setup lol
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+        ImGui::Begin("Settings");
+        ImGui::Text("Rotation");
+        ImGui::InputFloat("Speed", &ang, 0.1f, 1.0f);
+        ImGui::SliderFloat("X", &x, -1.0f, 1.0f);
+        ImGui::SliderFloat("Y", &y, -1.0f, 1.0f);
+        ImGui::SliderFloat("Z", &z, -1.0f, 1.0f);
+        ImGui::Text("Position");
+        ImGui::SliderFloat("PX", &px, -1.0f, 1.0f);
+        ImGui::SliderFloat("PY", &py, -1.0f, 1.0f);
+        ImGui::SliderFloat("PZ", &pz, -1.0f, 1.0f);
+        ImGui::Text("Movement");
+        ImGui::InputFloat("Player Speed", &movSpeed, 0.1f, 1.0f);
+        ImGui::Text("Camera Settings");
+        ImGui::InputFloat("FOV", &fov, 0.1f, 1.0f);
+        ImGui::Checkbox("WireFrame", &wireFrame);
+        ImGui::End();
+        if (fov >= 120.0f) {
+            fov = 120.0f;
+        }
+        if (fov <= 30.0f) {
+            fov = 30.0f;
+        }
+        if (wireFrame == true) {
+            wf = 0x1B01;
+        }
+        if (wireFrame == false) {
+            wf = 0x1B02;
+        }
+        //Linear algebra bs
+        auto model         = glm::mat4(1.0f);
+        glm::mat4 view = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
+        glm::mat4 projection;
+        glm::mat4 trans;
+        trans = translate(model, glm::vec3(px, py, pz));
+        auto time = (float)glfwGetTime();
+        float delta = time - lastFrme;
+        lastFrme = time;
+        currentAngle += ang * delta;
+        model = rotate(model, currentAngle, glm::vec3(x, y, z));
+        view  = translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+        projection = glm::perspective(glm::radians(fov), (float)SWIDTH / (float)SHEIGHT, 0.1f, 100.0f);
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
+        unsigned int viewLoc  = glGetUniformLocation(ourShader.ID, "view");
+        unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+        glClearColor(0.2, 0.1, 1.0, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        ourShader.use();
+        glUniformMatrix4fv((int)transformLoc, 1, GL_FALSE, &trans[0][0]);
+        glUniformMatrix4fv((int)modelLoc, 1, GL_FALSE, &model[0][0]);
+        glUniformMatrix4fv((int)viewLoc, 1, GL_FALSE, &view[0][0]);
+        glUniformMatrix4fv((int)projectionLoc, 1, GL_FALSE, &projection[0][0]);
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+        glPolygonMode(GL_FRONT_AND_BACK, wf);
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glDeleteVertexArrays(1, &vao);
+    glDeleteBuffers(1, &vbo);
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
+void input(GLFWwindow* window) {
+    float cameraSpeed = movSpeed * deltaTime;
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= cameraRight * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += cameraRight * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+        cameraPos += cameraSpeed * cameraUp;
+    }
+    if (glfwGetKey(window, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS) {
+        cameraPos -= cameraSpeed * cameraUp;
+    }
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
